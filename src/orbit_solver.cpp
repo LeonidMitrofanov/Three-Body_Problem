1dc4c7b167fc2fe7d7208cab5a132f1c63472bcd
Leonid_MacBook <alforfon@gmail.com>
2025-04-01 14:14:10 +0300

    [refactor] divided into 2 parts

--- src/orbit_solver.cpp
+++ src/orbit_solver.cpp
@@ -6,17 +6,6 @@
 
 using namespace std;
 
-// Функция для тестовой задачи
-vector<double> testSystem(double t, const vector<double>& y) {
-    double y1 = y[0];
-    double y2 = y[1];
-
-    double dy1dt = -y2 + y1 * (y1 * y1 + y2 * y2 - 1);
-    double dy2dt = y1 + y2 * (y1 * y1 + y2 * y2 - 1);
-
-    return {dy1dt, dy2dt};
-}
-
 // Функция для основной задачи (трёх тел)
 vector<double> threeBodySystem(double t, const vector<double>& y) {
     double x = y[0];
@@ -39,36 +28,7 @@ vector<double> threeBodySystem(double t, const vector<double>& y) {
 }
 
 int main() {
-    // Решение тестовой задачи
-    {   
-        const char test_path[] = "../data/test.csv";
-        vector<double> y_test = {1.0 / sqrt(2), 0.0}; // Начальные условия для тестовой задачи
-        double t_test = 0.0;
-        double T_test = 5.0; // Конечное время для тестовой задачи
-        double h_test = 0.01; // Шаг интегрирования
 
-        // Создаём решатель для тестовой задачи
-        RungeKutta4 solver_test(testSystem, y_test, h_test);
-
-        // Открываем файл для сохранения траектории
-        ofstream file_test(test_path);
-        file_test << "t,y1,y2\n";
-
-        // Интегрируем уравнения до времени T_test
-        while (t_test < T_test) {
-            file_test << t_test << "," << y_test[0] << "," << y_test[1] << "\n";
-
-            solver_test.step(t_test);
-            y_test = solver_test.getState();
-            t_test += solver_test.getStepSize();
-        }
-
-        file_test.close();
-        cout << "Тестовая задача: Данные сохранены в " << test_path << endl;
-    }
-
-    // Решение задачи трёх тел
-    {
         const char orbit_path[] = "../data/three_body_orbit.csv";
         vector<double> y_three_body = {0.994, 0.0, 0.0, -2.031732629557337}; // Начальные условия для задачи трёх тел
         double t_three_body = 0.0;
@@ -94,7 +54,7 @@ int main() {
 
         file_three_body.close();
         cout << "Задача трёх тел: Данные сохранены в " << orbit_path << endl;
-    }
+
 
     return 0;
 }
